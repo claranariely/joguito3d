7bb9c678c266e4797990ded313659fc26f0c6db8
CLARA NARIELY DE MELO ALVES <20211170150067@ifrn.local>
2023-10-25 15:28:04 -0300

    rotacionando movimento

--- joguito3dclara/Assets/scripts/Player.cs
+++ joguito3dclara/Assets/scripts/Player.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private CharacterController controller;
-
     public float speed;
+
+    private Transform cam;
+    
+    public float smoothRotTime;
+
+    private float turnSmoothVelocity;
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        cam = Camera.main.transform;
     }
 
     // Update is called once per frame
@@ -23,7 +29,15 @@ public class Player : MonoBehaviour
 
         if (direction.magnitude > 0)
         {
-            controller.Move(direction * speed * Time.deltaTime);
+            float angle  = Mathf.Atan2(direction.x,direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, angle, ref turnSmoothVelocity, smoothRotTime);
+            
+            transform.rotation = Quaternion.Euler(0f, smoothAngle, 0f);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, angle, 0) * Vector3.forward;
+            
+            controller.Move(moveDirection * speed * Time.deltaTime);
+            
         }
     }
 }
