e579e1f08984599fe902bcf7606e444d6c02275e
CLARA NARIELY DE MELO ALVES <20211170150067@ifrn.local>
2023-10-25 14:48:23 -0300

    movimentando

--- joguito3dclara/Assets/scripts/Player.cs
+++ joguito3dclara/Assets/scripts/Player.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-private 
+    private CharacterController controller;
+
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical);
+
+        if (direction.magnitude > 0)
+        {
+            controller.Move(direction * speed * Time.deltaTime);
+        }
     }
 }
